5cb05a907e25f7e309939f9eb52f40c8e61548d8
chaewonlover0727 <omgnice147017@gmail.com>
2025-02-14 16:11:23 +0900

    오류수정1

--- routes/admin.js
+++ routes/admin.js
@@ -65,26 +65,98 @@ router.post(
     })
 );
 
+router.get(
+    "/allPosts",
+    checkLogin,
+    asyncHandler(async(req,res)=>{
+        const locals={
+            title:"Posts",
+        };
+        const data=await Post.find().sort({updateAt:"desc", createdAt:"desc"}); // 전체 게시물 가져오기
+        res.render("admin/allPosts",{ // locals값과 data 넘기기
+            locals,
+            data,
+            layout:adminLayout,
+        });
+    })
+);
+
+
+
 // 로그아웃
 router.get("/logout", (req, res) => {
     res.clearCookie("token");
     res.redirect("/");
 });
 
-// 게시물 목록
 router.get(
-    "/allPosts",
+    "/add",
     checkLogin,
-    asyncHandler(async (req, res) => {
-        const locals = { title: "Posts" };
-        const data = await Post.find().sort({ updateAt: "desc", createdAt: "desc" });
-
-        res.render("admin/allPosts", {
+    asyncHandler(async (req,res)=>{
+        const locals={
+            title:"게시물 작성",
+        };
+        res.render("admin/add",{
             locals,
-            data,
             layout: adminLayout,
         });
     })
 );
 
-module.exports = router;
+router.post(
+    "/add",
+    checkLogin,
+    asyncHandler(async (req,res)=>{
+        const {title, body}=req.body;
+
+        const newPost=new Post({
+            title:title,
+            body:body,
+        });
+
+        await Post.create(newPost);
+        res.redirect("/allPosts");
+    })
+);
+
+router.get(
+    "/edit/:id",
+    checkLogin,
+    asyncHandler(async (req,res)=>{
+        const locals={
+            title:"게시물 편집",
+        };
+        //id 값을 사용해서 게시물 가져오기
+        const data=await Post.findOne({ _id:req.params.id});
+        res.render("admin/edit", {
+            locals,
+            data,
+            layout:adminLayout,
+        });
+    })
+);
+
+router.put(
+    "/edit/:id",
+    checkLogin,
+    asyncHandler(async (req,res)=>{
+        await Post.findByIdAndUpdate(req.params.id,{
+            title:req.body.title,
+            body:req.body.body,
+            createdAt:Date.now(),
+        });
+        // 수정한 후 전체 목록 다시 표시하기
+        res.redirect("/allPosts");
+    })
+);
+
+router.delete(
+    "/delete/:id",
+    checkLogin,
+    asyncHandler(async (req,res)=>{
+        await Post.deleteOne({ _id: req.params.id});
+        res.redirect("/allPosts");
+    })
+);
+
+module.exports=router;
